d6d0246601abd5f2f84b83c185905ec7f8d52883
lution <lution@bydell.com>
2015-12-27 02:09:57 +0800

    init commit

---
+++ api/cloudflare.lua
@@ -0,0 +1,104 @@
+--
+-- CLoudflare DNS API
+-- Date:2015-12-25
+--
+local json = require('utils.json')
+
+function process(auth, record, ip)
+    local identifier = {}
+    identifier.zone = getZone(auth, record)
+    if identifier.zone then
+        identifier.record, data = getRecord(auth, identifier.zone, record)
+        if identifier.record then
+            local result = updateRecord(auth, identifier, ip, data)
+        end
+    end
+
+end
+
+--
+-- List all zones of this account
+-- see https://api.cloudflare.com/#zone-list-zones
+--
+
+function getZone(auth, record)
+    local raw_resp = ngx.location.capture(
+    '/cloudflare/zones',
+    { vars = {email = auth.email, api_key = auth.API_key} }
+    )
+    -- FIXME resp status check
+    local data, err = json:decode(raw_resp.body)
+    if not err then
+        if data.success == true then
+            for _, zone in pairs(data.result) do 
+                local a, b = #zone.name, #record
+                if b > a then
+                    local tmp = string.sub(record, b - a + 1, b)
+                    if (tmp == zone.name) then
+                        return zone.id
+                    end
+                end
+            end
+            return nil
+        else
+            showError(data.errors)
+        end
+    else
+        -- FIXME error message
+        return nil
+    end
+end
+
+--
+-- List DNS records of a specific zone
+-- see https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
+--
+
+function getRecord(auth, zone, record)
+    local raw_resp = ngx.location.capture(
+    '/cloudflare/zones/' .. zone .. '/dns_records?name=' .. record,
+    { vars = {email = auth.email, api_key = auth.API_key} }
+    )
+    -- FIXME resp status check
+    local data, err = json:decode(raw_resp.body)
+    if not err then
+        if data.success == true then
+            return data.result[1].id, data
+        else
+            showError(data.errors)
+        end
+    else
+        -- FIXME error message
+        return nil
+    end
+end
+
+--
+-- Update the DDNS record
+-- see https://api.cloudflare.com/#dns-records-for-a-zone-update-dns-record
+--
+
+function updateRecord(auth, identifier, ip, data)
+    data.result[1].content = ip
+    local raw_resp = ngx.location.capture(
+    -- FIXME modified_on not modified
+    '/cloudflare/zones/' .. identifier.zone .. '/dns_records/' .. identifier.record,
+    { vars = {email = auth.email, api_key = auth.API_key}, method = ngx.HTTP_PUT, body = json:encode(data.result[1]) }
+    )
+    -- FIXME resp status check
+    local data, err = json:decode(raw_resp.body)
+    if not err then
+        if data.success == true then
+            ngx.say("Success")
+        else
+            showError(data.errors)
+        end
+    else
+        -- FIXME error message
+        return nil
+    end
+end
+
+function showError(data)
+    ngx.say(json:encode(data))
+end
---
+++ index.lua
@@ -0,0 +1,43 @@
+require('utils.base64')
+require('utils.json')
+
+
+--
+-- Get Email and API key using basic authentacation
+--
+
+local auth = {}
+local record = "test.bydell.com"
+local ip = ngx.var.remote_addr
+local auth_header = ngx.var.http_Authorization
+
+if not auth_header then
+    ngx.status = 401
+    ngx.header["WWW-Authenticate"] = 'Basic realm="Welcome,Please log in."'
+    ngx.say('Not Authorized')
+    ngx.exit(ngx.HTTP_OK)
+else
+    -- Auth Header value looks like "Basic Og=="
+    local raw_auth_data = string.sub(ngx.var.http_Authorization, #"Basic " + 1, #ngx.var.http_Authorization)
+    local auth_data = base64_dec(raw_auth_data)
+    -- Original pattern from http://stackoverflow.com/questions/21040325/email-address-validation-using-corona-sdk
+    auth.email = string.match(auth_data, '^([A-Za-z0-9%.%%%+%-]+@[A-Za-z0-9%.%%%+%-]+%.%w%w%w?%w?):.*')
+    if not auth.email then
+        ngx.status = 401
+        ngx.header["WWW-Authenticate"] = 'Basic realm="Please provide a valid email address as username"'
+        ngx.exit(ngx.HTTP_OK)
+    end
+    auth.API_key = string.match(auth_data, "^[A-Za-z0-9%.%%%+%-]+@[A-Za-z0-9%.%%%+%-]+%.%w%w%w?%w?:(.*)$")
+    if not auth.API_key then
+        ngx.status = 401
+        ngx.header["WWW-Authenticate"] = 'Basic realm="Please provide your API key as password"'
+        ngx.exit(ngx.HTTP_OK)
+    end
+end
+
+--
+-- Call target API to process request
+--
+
+local test = require('api.cloudflare')
+process(auth, record, ip)
